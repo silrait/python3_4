8c2cd7cd56f2580b5c689e558db1379800c16e14
silrait <thiarllisba@hotmail.com>
2019-03-17 01:00:00 -0300

    Definições de herança, duck typing, properties, setters e magic methods.

--- modelo.py
+++ modelo.py
@@ -1,43 +1,100 @@
 class Programa:
     def __init__(self, nome, ano):
-        self.__nome = nome.title()
-        self.__ano = ano
-        self.__like = 0
+        self._nome = nome.title()
+        self._ano = ano
+        self._likes = 0
 
     @property
     def likes(self):
-        return self.__likes
+        return self._likes
 
-    def dar_likes(self):
-        self.__likes += 1
+    def dar_like(self):
+        self._likes += 1
 
     @property
     def nome(self):
-        return self.__nome
+        return self._nome
 
     @nome.setter
     def nome(self, nome):
-        self.__nome = nome
+        self._nome = nome
 
     @property
     def ano(self):
-        return self.__ano
+        return self._ano
+
+    def __str__(self):
+        return f'{self._nome} - {self._ano} - {self._likes} likes'
 
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        Programa.__init__(self, nome, ano)
+        super().__init__(nome, ano)
         self.__duracao = duracao
+
+    @property
+    def duracao(self):
+        return self.__duracao
+
+    def __str__(self):
+        return f'{self._nome} - {self._ano} - {self.__duracao} min - {self._likes} likes'
 
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        Programa.__init__(self, nome, ano)
+        super().__init__(nome, ano)
         self.__temporadas = temporadas
+
+    @property
+    def temporadas(self):
+        return self.__temporadas
+
+    def __str__(self):
+        return f'{self._nome} - {self._ano} - {self.__temporadas} temporadas - {self._likes} likes'
+
+
+class Playlist:
+    def __init__(self, nome, programas):
+        self._nome = nome
+        self._programas = programas
+
+    def __getitem__(self, item):
+        return self._programas[item]
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, nome):
+        self._nome = nome
+
+    @property
+    def listagem(self):
+        return self._programas
+
+    def __len__(self):
+        return len(self._programas)
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-print(vingadores.nome)
-
 atlanta = Serie('atlanta', 2018, 2)
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano}')
+tmep = Filme('Todo mundo em panico', 1999, 100)
+demolidor = Serie('Demolidor', 2016, 2)
+
+vingadores.dar_like()
+vingadores.dar_like()
+tmep.dar_like()
+demolidor.dar_like()
+demolidor.dar_like()
+demolidor.dar_like()
+atlanta.dar_like()
+
+filmes_e_series = [vingadores, atlanta, demolidor, tmep]
+playlist_fim_de_semana = Playlist('Fim de Semana', filmes_e_series)
+
+for programa in playlist_fim_de_semana:
+    print(programa)
+
+print(f'Tamanho: {len(playlist_fim_de_semana)}')
+print(vingadores in playlist_fim_de_semana)
